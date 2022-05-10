ebcc19b1927c7acb5b3579c16a58223ba2dfd3f6
HomoHikka <halfideas31@gmail.com>
2022-05-10 06:28:01 +0300

    - initial commit

---
+++ src/consts/AnimationKeys.ts
@@ -0,0 +1,9 @@
+enum AnimationKeys {
+    RocketMouseRun = 'rocket-mouse-run',
+    RocketFlamesOn = 'rocket-flame-on',
+    RocketMouseFall = 'rocket-mouse-fall',
+    RocketMouseFly = 'rocket-mouse-fly',
+    RocketMouseDead = 'rocket-mouse-dead',
+}
+
+export default AnimationKeys;
